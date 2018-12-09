14fc3baf61c469f949d8d2e317702e983f83745f
Subhrajit <subhrajit1997@gmail.com>
2018-12-09 09:29:18 +0530

    added README, lane, lane_video

---
+++ README.md
@@ -0,0 +1,17 @@
+# Shravan
+A "eye" for blind
+
+---
+
+## Instructions
+
+- Download and setup tensorflow models
+	- We need to use only object_detection part, so downloading only that will suffice
+	- Installation for object_detection [link](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md)
+	- We have to download protoc binary for windows separately
+- Install all the requirements `pip install -r requirements.txt`
+- Change all the IP addresses to match your device configurations
+- Run `location.py` on your phone to avail GPS functions.
+- Run `listener.py` on your system to fetch GPS data from the phone
+- Run the program using `main.py`
+- Speak one of the commands
--- lane.py
+++ lane.py
@@ -1,32 +1,44 @@
 import cv2
 import numpy as np
- 
-video = cv2.VideoCapture("test3.mp4")
- 
+
+video = cv2.VideoCapture("test2.1.mp4")
+flag=0
+streak=0
 while True:
-    ret, orig_frame = video.read()
-    if not ret:
-        video = cv2.VideoCapture("test3.mp4")
-        continue
- 
-    frame = cv2.GaussianBlur(orig_frame, (5, 5), 0)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    low_yellow = np.array([18, 94, 140])
-    up_yellow = np.array([48, 255, 255])
-    mask = cv2.inRange(hsv, low_yellow, up_yellow)
-    edges = cv2.Canny(mask, 50, 150)
- 
-    lines = cv2.HoughLinesP(edges, 2, (np.pi)/180, 100, maxLineGap=50)
-    if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
- 
-    cv2.imshow("frame", frame)
-    cv2.imshow("edges", edges)
- 
-    key = cv2.waitKey(25)
-    if key == 27:
-        break
+	ret, orig_frame = video.read()
+	if not ret:
+		video = cv2.VideoCapture("test2.1.mp4")
+		continue
+	frame = cv2.GaussianBlur(orig_frame, (5, 5), 0)
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	low_yellow = np.array([18, 94, 140])
+	up_yellow = np.array([48, 255, 255])
+	mask = cv2.inRange(hsv, low_yellow, up_yellow)
+	edges = cv2.Canny(mask, 50, 150)
+	#print(cv2.HOUGH_PROBABILISTIC)
+	lines = cv2.HoughLinesP(edges, 1, (np.pi)/180, 100, maxLineGap=50)
+	if lines is not None:
+		for line in lines:
+			x1, y1, x2, y2 = line[0]
+			cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+			vari=((y2-y1)/(x2-x1))
+			if streak>=3:
+				flag=1
+
+			if(vari<-0.5 or flag == 1):
+			   print('straight')
+			   streak=0
+			   continue
+
+			print('right')
+			streak+=1
+
+
+	cv2.imshow("frame", frame)
+	cv2.imshow("edges", edges)
+
+	key = cv2.waitKey(25)
+	if key == 27:
+		break
 video.release()
 cv2.destroyAllWindows()
