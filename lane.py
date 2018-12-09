d2dacc8dbb30ee053db3af6e832babc3d05d2280
Subhrajit <subhrajit1997@gmail.com>
2018-12-09 10:16:19 +0530

    fixed some inputs, added frame delay

--- lane.py
+++ lane.py
@@ -1,13 +1,29 @@
 import cv2
 import numpy as np
+import pyttsx3 as p
+import time
+
+def speak(s):
+	engine =p.init()
+	engine.say(s)
+	engine.runAndWait()
+	time.sleep(1)
 
 video = cv2.VideoCapture("lane_video.mp4")
+flag = 0
 
 while True:
 	ret, orig_frame = video.read()
 	if not ret:
-		video = cv2.VideoCapture("test2.1.mp4")
+		video = cv2.VideoCapture("lane_video.mp4")
 		continue
+
+	if flag <= 100:
+		flag+=1
+		continue
+	else:
+		flag = 0
+	
 	frame = cv2.GaussianBlur(orig_frame, (5, 5), 0)
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	low_yellow = np.array([18, 94, 140])
@@ -17,23 +33,23 @@ while True:
 	#print(cv2.HOUGH_PROBABILISTIC)
 	lines = cv2.HoughLinesP(edges, 1, (np.pi)/180, 100, maxLineGap=50)
 	if lines is not None:
-		for line in lines:
-			x1, y1, x2, y2 = line[0]
-			cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
-			vari=((y2-y1)/(x2-x1))
-			
-			if ( vari<-0.5 ):
-			   print('straight')			   
-			   continue
+		line = lines[0]
+		x1, y1, x2, y2 = line[0]
+		cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+		vari=((y2-y1)/(x2-x1))
+		
+		if ( vari<-0.5 ):
+		   print('straight')			   
+		   continue
 
-			print('right')
+		speak('right')
 			
 
 	cv2.imshow("frame", cv2.resize(frame, (640, 480)))
 	# cv2.imshow("edges", cv2.resize(edges, (640, 480)))
 
-	key = cv2.waitKey(25)
-	if key == 27:
+	if cv2.waitKey(25) & 0xFF == ord('q'):
+		cv2.destroyAllWindows()
 		break
 video.release()
 cv2.destroyAllWindows()
